936736c727ad5ae9f54f32d132a333e324418e44
Arturo Ezequiel Gonzalez <112989145+caeaeg@users.noreply.github.com>
2022-09-21 11:36:44 -0300

    tp2

---
+++ tp2/ej 4.js
@@ -0,0 +1,20 @@
+<html>
+
+
+
+
+    <scrip type="text,javascript">/
+        var numero = prompt("INGRESE UN NUMERO")
+        var resultado = parimpar(numero);
+        alert("EL NUMERO "+numero+"ES "+resultado);
+
+        function parimpar(numero);
+        if(numero % 2 == 0) {
+    return "par";
+}
+        else{
+    return "impar";
+}
+    </scrip>
+
+</html>
---
+++ tp2/ej3.js
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+        <title>ejercicio n°3</title>
+    </head>
+
+    <body>
+        <script>
+
+            var numero = prompt("INTRODUZCA UN NUMRO PARA CALCULAR EL FACTORIAL");
+            var resultado = 1
+
+            for(varn i=1; i<numero; i++) {
+                resultado *= i;
+            }
+
+            alert(resultado)
+        </script>
+
+    </body>
+</html>
