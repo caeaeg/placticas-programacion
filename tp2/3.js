d946d7f983780a53bdcb199efa6e85b1dd54568a
Arturo Ezequiel Gonzalez <112989145+caeaeg@users.noreply.github.com>
2022-09-27 10:13:22 -0300

    tp2

---
+++ tp2/3.js
@@ -0,0 +1,23 @@
+//ejercicio 3
+<html>
+
+    <head>
+        <title>ejercicio n°3</title>
+    </head>
+
+    <body>
+
+        <script type="text/javascript">
+            var numero = prom
+
+
+
+
+
+
+        </script>
+
+    </body>
+
+
+
--- tp2/ej2.js
+++ tp2/ej2.js
@@ -14,7 +14,7 @@ console.log("num2 es positivo");
 if(num1 =< 0) {
 console.log("num1 es negativo o distinto de cero");
 }
-if((num1+1)<num2) {
+if((num1+5)<num2) {
 console.log("Incrementar en 1 unidad el valor de num1 no lo hace mayor o igual que num2"); }
 
 
